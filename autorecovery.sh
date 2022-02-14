b21162adc54428a2e4d0cacd927b674c5c8d1492
CheesiestMaster <74749672+CheesiestMaster@users.noreply.github.com>
2022-02-14 13:36:27 -0500

    Switch recoveryconf to a relative path

--- autorecovery.sh
+++ autorecovery.sh
@@ -3,7 +3,7 @@ while : ;do
 echo "python start"
 ./bot.py
 echo "python end"
-sleep=$(head -1 /home/jason/bots/dnd/recoverconf)
+sleep=$(head -1 ./recoverconf)
 echo "sleep start $sleep"
 sleep "$sleep"
 done
